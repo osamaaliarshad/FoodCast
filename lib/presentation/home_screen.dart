1cfed648a7e140ecbf5aa5e3ddae405eae9ce775
MonacledPotato <oaliarshad@gmail.com>
2021-01-22 16:29:38 -0800

    Added interactive UI elements

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -4,88 +4,115 @@ import 'package:flutter_riverpod/all.dart';
 const Color activeColor = Color(0xFF192248);
 const Color deactiveColor = Color(0xFFB9B9C0);
 
-class MyHomePage extends ConsumerWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context, watch) {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 1;
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Color(0xFFFBFAFD),
-      body: Center(
-        child: Row(
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width / 6,
-                  color: Color(0xFFFDF0E2),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      RotatedBox(
-                        quarterTurns: 0,
-                        child: Icon(
-                          Icons.reorder,
-                          color: deactiveColor,
-                        ),
-                      ),
-                      RotatedBox(
-                        quarterTurns: 0,
-                        child: Icon(
-                          Icons.search,
-                          color: deactiveColor,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          print('object');
-                        },
-                        child: RotatedBox(
-                          quarterTurns: 3,
-                          child: Text(
-                            'Calendar',
-                            style: TextStyle(color: deactiveColor),
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          print('object2');
-                        },
-                        child: RotatedBox(
-                          quarterTurns: 3,
-                          child: Text(
-                            'Today',
-                            style: TextStyle(color: activeColor),
-                          ),
-                        ),
-                      ),
-                      RotatedBox(
-                        quarterTurns: 3,
-                        child: Text(
-                          'Recipes',
-                          style: TextStyle(color: deactiveColor),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      RotatedBox(
-                        quarterTurns: 3,
-                        child: Icon(
-                          Icons.settings,
-                          color: deactiveColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
+      body: Row(
+        children: <Widget>[
+          NavigationRail(
+            minWidth: width / 8,
+            groupAlignment: 1.0,
+            backgroundColor: Color(0xFFFDF0E2),
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            labelType: NavigationRailLabelType.all,
+            destinations: [
+              buildRotatedTextRailDestination("Calendar"),
+              buildRotatedTextRailDestination("Today"),
+              buildRotatedTextRailDestination("Recipes"),
+            ],
+            trailing: Column(
+              children: [
+                SizedBox(
+                  height: height / 8,
+                ),
+                SizedBox(
+                  height: height / 105,
+                ),
+                IconButton(icon: Icon(Icons.tune), onPressed: () {})
               ],
             ),
-          ],
+            selectedLabelTextStyle: TextStyle(
+              color: activeColor,
+              letterSpacing: 0.8,
+            ),
+            unselectedLabelTextStyle: TextStyle(
+              color: deactiveColor,
+              letterSpacing: 0.8,
+            ),
+          ),
+
+          // VerticalDivider(
+          //   thickness: 1,
+          //   width: 1,
+          //   color: Colors.black,
+          // ),
+
+          // This is the main content.
+          ContentSpace(_selectedIndex)
+        ],
+      ),
+    );
+  }
+}
+
+NavigationRailDestination buildRotatedTextRailDestination(String text) {
+  return NavigationRailDestination(
+    icon: SizedBox.shrink(),
+    label: Padding(
+      padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+      child: RotatedBox(
+        quarterTurns: 3,
+        child: Text(text),
+      ),
+    ),
+  );
+}
+
+class ContentSpace extends StatelessWidget {
+  final int _selectedIndex;
+  ContentSpace(this._selectedIndex);
+
+  final List<String> titles = [
+    "Calendar",
+    "Today's\nFood",
+    "All\nRecipes",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(24, 8, 0, 0),
+        child: MediaQuery.removePadding(
+          removeTop: true,
+          context: context,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: 24,
+              ),
+              Text(titles[_selectedIndex],
+                  style: Theme.of(context).textTheme.headline4),
+              SizedBox(
+                height: 24,
+              ),
+            ],
+          ),
         ),
       ),
     );
