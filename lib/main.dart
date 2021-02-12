c9cc1f41eb53e125f44ca5e1edf1aab09182b56b
MonacledPotato <oaliarshad@gmail.com>
2021-02-12 15:04:31 -0800

    Cleaner fix of authentication flow, issues persist

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/all.dart';
+import 'package:foodcast/providers/auth_providers.dart';
+import 'package:foodcast/views/authentication_views/authentication_wrapper.dart';
+import 'package:foodcast/views/authentication_views/login_page.dart';
 import 'package:foodcast/views/home_page.dart';
-import 'package:foodcast/views/login_page.dart';
-import 'package:foodcast/views/signup_page.dart';
 import 'constants.dart';
-import 'landing_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       systemNavigationBarIconBrightness: Brightness.dark,
@@ -23,7 +24,6 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations(
@@ -31,23 +31,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: sidebarColor, fontFamily: 'Spartan'),
-      home: FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('something went wrong'),
-            );
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            return LandingPage();
-          }
-          return Scaffold(
-              body: Center(
-            child: CircularProgressIndicator(),
-          ));
-        },
-      ),
+      home: AuthenticationWrapper(),
     );
   }
 }
